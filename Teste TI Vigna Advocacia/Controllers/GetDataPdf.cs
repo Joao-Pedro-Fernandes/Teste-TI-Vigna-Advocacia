c3fec7bd2927a55d1579c401693dfbf8340ce472
Joao-Pedro-Fernandes <113483050+Joao-Pedro-Fernandes@users.noreply.github.com>
2025-02-27 00:05:46 -0300

    Commit Inicial

---
+++ Teste TI Vigna Advocacia/Controllers/GetDataPdf.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using Microsoft.AspNetCore.Mvc;
+using Teste_TI_Vigna_Advocacia.Services;
+
+namespace Teste_TI_Vigna_Advocacia.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GetDataPdf : ControllerBase
+    {
+        private readonly GroqService _groqService;
+
+        public GetDataPdf(GroqService groqService)
+        {
+            _groqService = groqService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadPdf(IFormFile file)
+        {
+            if (file == null)
+            {
+                return BadRequest("Não foi possível processar o seu arquivo.");
+            }
+
+            try
+            {
+                using var memoryStream = new MemoryStream();
+                await file.CopyToAsync(memoryStream);
+                memoryStream.Position = 0;
+
+                using var pdfReader = new PdfReader(memoryStream);
+                using var pdfDocument = new PdfDocument(pdfReader);
+
+                StringBuilder text = new StringBuilder();
+                
+                for (int i = 1; i <= pdfDocument.GetNumberOfPages(); i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(pdfDocument.GetPage(i)));
+                }
+
+                var response = await _groqService.ResponseGroqPdfAnalyze(text.ToString());              
+
+                return Ok(_groqService.ExtractJson(response));
+                
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro: {ex.Message}");
+            }
+
+        }
+
+    }
+}
---
+++ Teste TI Vigna Advocacia/Services/GroqService.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace Teste_TI_Vigna_Advocacia.Services
+{
+    public class GroqService
+    {
+        private readonly HttpClient _httpClient;
+        private const string UrlGroq = "https://api.groq.com/openai/v1/chat/completions";
+        private const string GroqApiKey = "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";
+
+        public GroqService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> ResponseGroqPdfAnalyze(string text)
+        {
+            var requestBody = new
+            {
+                model = "llama-3.3-70b-versatile",
+                messages = new[]
+                {
+                    new { role = "user", content = "Estou te enviando em formato de texto um processo jurídico" +
+                    " que antes estava em formato de pdf." +
+                    " Você deve fazer a extração do número do processo e das partes envolvidas." +
+                    " Quero que na sua resposta seja em formato de json, da seguinte forma (exemplo):" +
+                    " { numero_processo: NUMERO_DO_PROCESSO_AQUI, partes: [João da Silva (réu), Banco XYZ (Autor), José (Advogado)]}." +
+                    " Sua resposta deve se limitar somente a este json." +
+                    $" quero que extraia do seguinte texto: {text}" }
+                }
+            };
+
+            var jsonRequest = JsonSerializer.Serialize(requestBody);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {GroqApiKey}");
+
+            var response = await _httpClient.PostAsync(UrlGroq, content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Erro na API Groq {response.StatusCode}");
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return responseContent;
+        }
+
+
+        public string ExtractJson(string json)
+        {
+            try
+            {
+                using var document = JsonDocument.Parse(json);
+                var root = document.RootElement;
+
+                string? jsonContent = root
+                    .GetProperty("choices")[0]
+                    .GetProperty("message")
+                    .GetProperty("content")
+                    .GetString();
+
+                if (string.IsNullOrEmpty(jsonContent))
+                    throw new Exception("Erro ao reconhecer o json");
+
+                using var jsonDocument = JsonDocument.Parse(jsonContent);
+                jsonDocument.RootElement.Clone();
+
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                };
+
+                return JsonSerializer.Serialize(jsonDocument, options);
+            }
+            catch (Exception ex)
+            {
+                return $"Erro ao processar a resposta da API {ex.Message}";
+            }
+        }
+
+    }
+}
